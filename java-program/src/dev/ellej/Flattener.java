acb527cb9d7b11fb59ad1698f8d00a85e4d135f6
Elle J <81748770+elle-j@users.noreply.github.com>
2021-04-02 08:49:03 +0200

    Add pseudo code for flattener

---
+++ java-program/src/dev/ellej/Flattener.java
@@ -0,0 +1,25 @@
+package dev.ellej;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Flattener {
+  public Map<String, ? extends Object> flatten(Map<String, ? extends Object> input) {
+    Map<String, ? super Object> output = new HashMap<>();
+    flatten("", input, output);
+
+    return output;
+  }
+  
+  private void flatten(String keyPath, Map<String, ? extends Object> input, Map<String, ? super Object> output) {
+    //  pseudo code:
+    //  for each key in the input map
+    //    if the key path is not at the root
+    //		  add a period to the path
+    //    add the key to the path
+    //	  if the value is a map
+    //		  flatten again from the updated path
+    //	  else
+    //		  add the path and the value to the result map
+  }
+}
---
+++ java-program/src/dev/ellej/Main.java
@@ -0,0 +1,8 @@
+package dev.ellej;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+  }
+}
