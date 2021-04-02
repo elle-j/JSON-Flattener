2239e71789180f3658fe8d76a77424bcfe1b3410
Elle J <81748770+elle-j@users.noreply.github.com>
2021-04-02 11:40:31 +0200

    Implement JSON helper

---
+++ java-program/src/dev/ellej/JSONHelper.java
@@ -0,0 +1,71 @@
+package dev.ellej;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JSONHelper {
+  private ScriptEngine engine;
+
+  public JSONHelper() {
+    engine = new ScriptEngineManager().getEngineByName("javascript");
+  }
+
+  public Map<String, ? extends Object> parse(String json) {
+    Map<String, ? extends Object> parsed = new HashMap<>();
+
+    try {
+      parsed = (Map) engine.eval("Java.asJSONCompatible(" + json + ")");
+    }
+    catch (ScriptException e) {
+      e.printStackTrace();
+    }
+
+    return parsed;
+  }
+
+  public String stringify(Map<String, ? extends Object> parsed) {
+    var result = new StringBuilder();
+
+    result.append("{");
+    stringify(parsed, result, 1);
+    result.append("\n}");
+
+    return result.toString();
+  }
+
+  private void stringify(Map<String, ? extends Object> parsed, StringBuilder result, int depth) {
+    int count = 0;
+    for (String key : parsed.keySet()) {
+      boolean isLast = ++count == parsed.size();
+
+      result.append("\n" + getIndentation(depth) + "\"" + key + "\": ");
+
+      Object value = parsed.get(key);
+      if (value instanceof Map) {
+        result.append("{");
+        stringify((Map) value, result, depth + 1);
+        result.append("\n" + getIndentation(depth) + "}");
+      }
+      else {
+        if (value instanceof String)
+          result.append("\"" + value + "\"");
+        else
+          result.append(value);
+
+        if (!isLast)
+          result.append(",");
+      }
+    }
+  }
+
+  private String getIndentation(int tabs) {
+    var indentation = new StringBuilder();
+    for (var i = 0; i < tabs; i++)
+      indentation.append("\t");
+
+    return indentation.toString();
+  }
+}
